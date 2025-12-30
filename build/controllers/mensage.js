53639536bac54ebc12b0b391dda80991423de4dd
Mikensonthomas142 <mikensonthomas2@gmail.com>
2025-12-30 20:55:51 -0300

    Co

---
+++ build/controllers/mensage.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.registerMessages = void 0;
+const prisma = require("../config/prisma").default;
+const registerMessages = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { name, email, telefone, message } = req.body;
+        const newMessage = yield prisma.messages.create({
+            data: {
+                name,
+                email,
+                telefone,
+                message
+            },
+        });
+        res.status(200).json(newMessage);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao registrar a mensagem" });
+    }
+});
+exports.registerMessages = registerMessages;

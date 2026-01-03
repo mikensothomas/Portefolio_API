417a3e4a1247747f40dfc0fe05d989f2291e0ada
Mikensonthomas142 <mikensonthomas2@gmail.com>
2026-01-03 01:31:39 -0300

    Commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,28 +1,3 @@
-// import { Router } from "express";
-// import { uploadAvatar } from "./middleware/upload/avatar";
-// import { registerProjects } from "./controllers/registerProjetoController";
-// import multer from "multer";
-// import { registerMessages } from "./controllers/mensage";
-// import { listProjects } from "./controllers/listProjectControllers";
-
-// const upload = multer(uploadAvatar.getConfig);
-// export const router = Router()
-
-// router.get("/", (_, res) => {
-//   res.json({
-//     status: "API funcionando 🚀",
-//     message: "Backend online no Render"
-//   });
-// });
-
-// router.post("/registerProjects", upload.fields([
-//     { name: "imagem_capa", maxCount: 1 },
-//     { name: "imagens", maxCount: 1000 },
-// ]), registerProjects)
-
-// router.get("/listProjects", listProjects)
-// router.post("/registerMessage", registerMessages)
-
 import { Router } from "express";
 import { uploadAvatar } from "./middleware/upload/avatar";
 import { registerProjects } from "./controllers/registerProjetoController";

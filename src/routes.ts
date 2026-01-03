ff720833629d5e57ef6e3a17290a637d3f8de2bb
Mikensonthomas142 <mikensonthomas2@gmail.com>
2026-01-03 17:03:02 -0300

    Commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,36 +1,4 @@
-// import { Router } from "express";
-// import { registerProjects } from "./controllers/registerProjetoController";
-// import { registerMessages } from "./controllers/mensage";
-// import { listProjects } from "./controllers/listProjectControllers";
-// import uploadCloudinary from "./middleware/uploads/uploadCloudinary";
-
-// const upload = uploadCloudinary;
-
-// const routes = Router();
-
-// routes.get("/", (_, res) => {
-//   res.json({
-//     status: "API funcionando 🚀",
-//     message: "Backend online no Render",
-//   });
-// });
-
-// routes.post(
-//   "/registerProjects",
-//   upload.fields([
-//     { name: "imagem_capa", maxCount: 1 },
-//     { name: "imagens", maxCount: 1000 },
-//   ]),
-//   registerProjects
-// );
-
-// routes.get("/listProjects", listProjects);
-// routes.post("/registerMessage", registerMessages);
-
-// export default routes;
-
 import { Router } from "express";
-// import uploadCloudinary from "./middleware/upload/uploadCloudinary";
 import { registerProjects } from "./controllers/registerProjetoController";
 import { listProjects } from "./controllers/listProjectControllers";
 import { registerMessages } from "./controllers/mensage";
